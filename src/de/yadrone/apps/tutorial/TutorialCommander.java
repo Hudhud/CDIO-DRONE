a54feabf09953f64f2e9bd991ac8620b000fa7c9
Hadi <hadihorani@hotmail.com>
2016-06-07 13:55:23 +0200

    eewewr

---
+++ src/de/yadrone/apps/tutorial/TutorialCommander.java
@@ -0,0 +1,73 @@
+package de.yadrone.apps.tutorial;
+
+import de.yadrone.base.IARDrone;
+import de.yadrone.base.command.CommandManager;
+import de.yadrone.base.command.LEDAnimation;
+
+public class TutorialCommander
+{
+
+	private IARDrone drone;
+
+	public TutorialCommander(IARDrone drone)
+	{
+		this.drone = drone;
+	}
+
+	public void animateLEDs()
+	{
+		drone.getCommandManager().setLedsAnimation(LEDAnimation.BLINK_ORANGE, 3, 10);
+	}
+	
+	public void takeOffAndLand()
+	{
+		drone.takeOff();
+		drone.getCommandManager().hover().doFor(5000);
+//		drone.getCommandManager().up(100).doFor(60000);
+//		drone.getCommandManager().hover().doFor(10000);
+		drone.getCommandManager().forward(100).doFor(3000);
+		System.out.println(drone.getSpeed());
+		
+		
+		//		drone.getCommandManager().up(30).doFor(1000);
+//		drone.getCommandManager().forward(30).doFor(1000);
+//		drone.getCommandManager().hover().doFor(2000);
+//		drone.getCommandManager().spinRight(100).doFor(2000);
+	//	drone.getCommandManager().landing();
+		
+	}
+	
+//	public void leftRightForwardBackward()
+//	{
+//		final CommandManager cmd = drone.getCommandManager();
+//		final int speed = 10; // percentage of max speed
+//			
+//		cmd.takeOff();
+//		cmd.up(speed).doFor(1000);
+//		
+////		cmd.goLeft(speed).doFor(1000);
+////		cmd.hover().doFor(2000);
+////		
+////		cmd.goRight(speed).doFor(1000);
+////		cmd.hover().doFor(2000);
+//		
+//		cmd.forward(speed).doFor(1000);
+////		cmd.hover().doFor(1000);
+//		
+//		
+////		cmd.backward(speed).doFor(2000);
+////		cmd.hover().doFor(2000);
+//		
+//		cmd.landing();
+//		
+//		// alternative: asynchronous call
+////		cmd.takeOff();
+////		cmd.schedule(5000, new Runnable() { // schedule to be executed in 5 secs
+////			public void run()
+////			{
+////				cmd.goLeft(speed);
+////				// [...]
+////			}			
+////		});
+//	}
+}
---
+++ src/de/yadrone/apps/tutorial/TutorialMain.java
@@ -0,0 +1,66 @@
+package de.yadrone.apps.tutorial;
+
+
+import de.yadrone.base.ARDrone;
+import de.yadrone.base.IARDrone;
+import de.yadrone.base.command.LEDAnimation;
+import de.yadrone.base.exception.ARDroneException;
+import de.yadrone.base.exception.IExceptionListener;
+import de.yadrone.base.navdata.AttitudeListener;
+
+public class TutorialMain
+{
+	private static IARDrone drone = null;
+	
+	public static void main(String[] args)
+	{
+
+		try
+		{
+			// Tutorial Section 1
+			drone = new ARDrone();
+			drone.addExceptionListener(new IExceptionListener() {
+				public void exeptionOccurred(ARDroneException exc)
+				{
+					exc.printStackTrace();
+				}
+			});
+			drone.getCommandManager().setMinAltitude(2500);
+			drone.getCommandManager().setMaxAltitude(3000);
+			drone.setSpeed(100);
+			drone.start();
+			
+			
+			// Tutorial Section 2
+			new TutorialAttitudeListener(drone);
+																									
+			// Tutorial Section 3
+		//	new TutorialVideoListener(drone);
+			
+			Thread t = new Thread() {
+				public void run() {
+					TutorialVideoListener video = new TutorialVideoListener(drone);
+					drone.getVideoManager().addImageListener(video.getvideo());
+				}
+			};
+			t.start();
+			
+			//Tutorial Section 4
+			TutorialCommander commander = new TutorialCommander(drone);
+			commander.animateLEDs();
+			commander.takeOffAndLand();
+//			commander.leftRightForwardBackward();
+		}
+		catch (Exception exc)
+		{
+			exc.printStackTrace();
+		}
+		finally
+		{
+			if (drone != null)
+				drone.stop();
+
+			System.exit(0);
+		}
+	}
+}
